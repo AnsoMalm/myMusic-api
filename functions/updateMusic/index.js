6ea1af71d13f7067d76e44b050e5f85896ae7ba6
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-09-27 12:00:54 +0200

    added new alternative to change, songtitle, group and both group and songtitle.

--- functions/updateMusic/index.js
+++ functions/updateMusic/index.js
@@ -16,30 +16,75 @@ export const handler = async (event) => {
 	try {
 		const group = JSON.parse(event.body)
 		const id = event.pathParameters.id; 
+		
+		let updateExpression = 'set'; 
+		let expressionAttributeValues = {}; 
+
+		//ändra songtitle
+		if (group.SongTitle) {
+			updateExpression += ' SongTitle = :s'; 
+			expressionAttributeValues[':s'] = group.SongTitle; 
+		}
+		//ändra gruppen
+		if (group.Group) {
+			updateExpression += ' Group = :g'; 
+			expressionAttributeValues[':g'] = group.Group; 
+		}
+		//ändra både gruppen och songtitle
+		if (group.SongTitle && group.Group) {
+			updateExpression += ',';
+		}
+		
 		await dynamo.send(
 			new UpdateCommand({
 				TableName: tableName, 
 				Key: {
 					"id": id
 				},
-				UpdateExpression: "set SongTitle = :s",
-				ExpressionAttributeValues: {
-					":s": group.SongTitle
-				}
+				UpdateExpression: updateExpression,
+				ExpressionAttributeValues: expressionAttributeValues, 
 			})
 		)
-		body = `Change songTitle ${id}`
+		body = `Change in songtitle and group ${id}`
+	} catch (error) {
+		statusCode = 400; 
+		body = error.message;
+	} finally {
+		body = JSON.stringify(body); 
+	}
+	return {
+		statusCode, 
+		body, 
+		headers, 
+	}
+}
+
+
+//Denna kod är för att kunna ändra songtitle i sin grupp. 
+// 		await dynamo.send(
+// 			new UpdateCommand({
+// 				TableName: tableName, 
+// 				Key: {
+// 					"id": id
+// 				},
+// 				updateExpression: "set SongTitle = :s",
+// 				ExpressionAttributeValues: {
+// 					":s": group.SongTitle
+// 				}
+// 			})
+// 		)
+// 		body = `Change songTitle ${id}`
 		
-	} catch (err) {
-		statusCode = 400;
-		body = err.message;
-	  } finally {
-		body = JSON.stringify(body);
-	  }
+// 	} catch (err) {
+// 		statusCode = 400;
+// 		body = err.message;
+// 	  } finally {
+// 		body = JSON.stringify(body);
+// 	  }
 	
-	  return {
-		statusCode,
-		body,
-		headers,
-	  };
-};
+// 	  return {
+// 		statusCode,
+// 		body,
+// 		headers,
+// 	  };
+// };
