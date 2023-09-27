beb1608a40938ca3245d14963da49d5eddf3959c
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-09-27 09:56:19 +0200

    set up my deletefunction and added getcommand inside the code

--- functions/deleteMusic/index.js
+++ functions/deleteMusic/index.js
@@ -1,19 +1,82 @@
-const {sendResponse, sendError } = require()
-const { db } = require('../../services/db'); 
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import {DynamoDBDocumentClient, DeleteCommand, GetCommand} from "@aws-sdk/lib-dynamodb"
+import {nanoid} from "nanoid"
 
+const client = new DynamoDBClient({}); 
+const dynamo = DynamoDBDocumentClient.from(client)
 
-exports.handler = async (event, context) => {
-	const { musicId } = event.pathParameters; 
+const tableName = "Music"; 
 
-	try {
-		await db.delete({
-			//TableName: ''
-			//Key: { id:  musicId }
-		}).promise()
-		
-		return sendResponse(200, {success: true }) 
-		
-	} catch (error) {
-		return sendError(500, {sucess: false, message: 'Could not remove music'  })
+export const handler = async (event) => {
+	let body; 
+	let statusCode = 200; 
+	const headers = {
+		"Content-type": "application/json"
 	}
-}
+	try {
+		const { Item } = await dynamo.send(
+			new GetCommand({
+				TableName: tableName, 
+				Key: {
+					id: event.pathParameters.id
+				}
+			})
+		)
+		if (!Item) {
+			throw new Error('ID not found')
+		}
+		await dynamo.send(
+			new DeleteCommand({
+				TableName: tableName, 
+				Key: {
+					id: event.pathParameters.id
+				}
+			})
+		)
+		body = `Deleted group ${event.pathParameters.id}`
+		
+	} catch (err) {
+		statusCode = 400;
+    	body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  } 
+  return {
+		statusCode,
+		body,
+		headers,
+	};
+};	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {sendResponse, sendError } = require()
+// const { db } = require('../../services/db'); 
+
+
+// exports.handler = async (event, context) => {
+// 	const { musicId } = event.pathParameters; 
+
+// 	try {
+// 		await db.delete({
+// 			//TableName: ''
+// 			//Key: { id:  musicId }
+// 		}).promise()
+		
+// 		return sendResponse(200, {success: true }) 
+		
+// 	} catch (error) {
+// 		return sendError(500, {sucess: false, message: 'Could not remove music'  })
+// 	}
+// }
